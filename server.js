09832e8795e66369d7693daca4d20e0a9f13c2c3
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-14 19:34:01 -0400

    set up socket.io server

--- server.js
+++ server.js
@@ -68,3 +68,10 @@ var server = http.createServer(function(request, response){
 server.listen(3000, function(){
     console.log("Server listening on port 3000");
 });
+
+/* Socket.io server */
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+
